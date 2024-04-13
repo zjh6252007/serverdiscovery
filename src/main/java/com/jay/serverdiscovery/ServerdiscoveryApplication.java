61ea4c9615715fb2ab8055faf24bdca62072bb3e
Jay <zjh6252007@gmail.com>
2024-04-12 22:17:27 -0400

    create server discovery

---
+++ src/main/java/com/jay/serverdiscovery/ServerdiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.jay.serverdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerdiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerdiscoveryApplication.class, args);
+    }
+
+}
